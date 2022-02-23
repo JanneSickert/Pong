9fcd6c88d1d703a83f51d8ea1b472b774aed58dc
Sickert <janne.sickert@irix.ch>
2022-02-23 15:41:42 +0100

    update main

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import GUI
-import Data
 import Berechnung
 
-data = Data.Data()
 gui = GUI.GUI()
 berechnung = Berechnung.Berechnung()
 
 gui.repaint(berechnung.getDataObject())
-
-print(data.court)
