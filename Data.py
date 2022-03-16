84c6b40e202bc9db5a3a299b723c011ea580f8cb
Sickert <janne.sickert@irix.ch>
2022-03-16 17:23:44 +0100

    clean all

--- Data.py
+++
@@ -1,5 +0,0 @@
-X_WIDTH = 1200                # Breite Spielfeld
-Y_HEIGHT = 750                 # Höhe Spielfeld
-PIXEL_SIZE = 25         # Höhe breite Pixel
-ELEMENTS_COLOR = "white"
-BACKGROUND_COLOR = "black"
